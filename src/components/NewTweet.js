b5e7529e4378c46c621587d97bca7096212eafef
kmostafa599 <kmostafa599@gmail.com>
2021-11-19 18:51:20 +0200

    Patch-2

--- src/components/NewTweet.js
+++ src/components/NewTweet.js
@@ -8,7 +8,7 @@ import React, { Component } from 'react'
 //set (add) #tweets data#
 // (get) #authed user# so the user can create new tweet
 
-//this component will be CONTAINER so it can dispatch an action from the STORE to let the reducers know that a new tweet has been made.
+//this component will be CONTAINER so it can dispatch an action from the STORE to let the reducers know that a new tweet has been made ,,, and also can read state from the store. --so we will use connect() method here
 
 class NewTweet extends Component {
   render() {
